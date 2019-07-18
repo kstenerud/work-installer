4bb6c98649803ade7a4d0954913e2478bfcfee67
Karl Stenerud <kstenerud@gmail.com>
2019-07-18 22:22:13 +0200

    Add x2go installer

---
+++ install-x2go.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+#####################################################################
+SCRIPT_HOME=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+source $SCRIPT_HOME/common.sh "$SCRIPT_HOME"
+
+show_after_help()
+{
+    echo "SSH Password authentication might be disabled by default. To enable it:"
+    echo "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
+    echo "systemctl restart sshd"
+    echo
+}
+
+install_crd()
+{
+    install_packages_from_repository ppa:x2go/stable \
+        x2goserver \
+        x2goserver-xsession \
+        x2goclient
+
+    show_after_help
+}
+
+#####################################################################
+
+install_crd
